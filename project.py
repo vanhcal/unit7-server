4e417821f79c79d9d2169953fbfb4c22d86568c1
vanhcal <mellychen87@gmail.com>
2017-01-11 00:08:54 -0800

    Add files via upload

---
+++ project.py
@@ -0,0 +1,43 @@
+# the same hello world function as before, but using flask
+from flask import Flask
+# create an instance of the Flask application with the name as the argument
+app = Flask(__name__)
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+# if we end up at either page/ or page/hello, the hello world function runs
+@app.route('/')
+# is a way of navigating to restaurants/1, restaurants/2, etc., given the restaurants_id parameter that is passed in
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurantMenu(restaurant_id):
+	# grab first restaurant from db
+	restaurant = session.query(Restaurant).first()
+	items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
+	output = ''
+	for i in items:
+		output += i.name
+		output += '</br>'
+		output += i.price
+		output += '</br>'
+		output += i.description
+		output += '</br>'
+	return output
+
+# restaurant_id will be passed in as the id for the new restaurant
+@app.route('/restaurant/<int:restaurant_id>/new/')
+def newMenuItem(restaurant_id):
+
+# the application run by the python interpreter is named main
+# imported python files get __name of file__
+# if you're running from python interpreter, run this function; if you're importing, don't do this function
+if __name__ == '__main__'
+	app.debug = True
+	app.run(host = '0.0.0.0', port = 5000)
